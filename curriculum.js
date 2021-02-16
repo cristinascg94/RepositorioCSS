7092381c6512635d3503e15a31049c7bced61938
cristinascg94 <cristinascg94@gmail.com>
2021-02-16 20:03:10 +0100

    16/2

--- curriculum.js
+++ curriculum.js
@@ -1,3 +1,18 @@
-function imprime(){
-    
-}
+const send= document.getElementsByTagName("button");
+console.log(send);
+send[0].addEventListener("click",envia);
+function envia(){
+    alert("Su mensaje ha sido enviado");
+};
+const texto= document.getElementById("msg");
+console.log(texto)
+texto.addEventListener("focusin",destaca)
+function destaca(){
+    console.log("hecho");
+    texto.style.backgroundColor= "#E0FFFF";
+};
+const texto2= document.getElementById("msg");
+texto.addEventListener("focusout",sinColor)
+function sinColor(){
+    texto.style.backgroundColor= "initial";
+};
